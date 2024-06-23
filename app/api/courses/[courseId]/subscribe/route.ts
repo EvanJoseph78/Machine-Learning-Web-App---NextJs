bde3826f9789f0d22e4acffb8e37954f5783be35
Evan Joseph <evandromariano49@gmail.com>
2024-06-23 10:15:06 -0300

    feat: fix for production - 2

--- app/api/courses/[courseId]/subscribe/route.ts
+++ app/api/courses/[courseId]/subscribe/route.ts
@@ -1,61 +1,61 @@
-
-import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs/server";
-
-// inscreve um usuário em um curso
-export async function POST(
-  req: Request,
-  { params }: { params: { courseId: string } }
-
-) {
-  try {
-
-    const { userId } = await req.json();
-    const { courseId } = params;
-
-    const existingUser = await db.users.findUnique({
-      where: {
-        userId: userId
-      },
-      include: {
-        subscribedCourses: true
-      }
-    })
-
-    if (!existingUser) return new NextResponse("usuário não cadastrado", { status: 200 });
-
-    // Verifica se o curso já está inscrito pelo usuário
-    const isCourseSubscribed = existingUser.subscribedCourses.some(course => course.courseId === courseId);
-    if (isCourseSubscribed) {
-      return new NextResponse("Usuário já está inscrito neste curso", { status: 409 });
-    }
-
-    const newSubscribedUser = await db.subscribedCourses.create({
-      data: {
-        courseId: courseId
-      }
-    })
-
-    const updateUser = await db.users.update({
-      where: { userId },
-      data: {
-        subscribedCourses: {
-          connect: {
-            id: newSubscribedUser.id,
-          }
-        }
-      }
-    })
-
-    return new NextResponse("created", { status: 200 });
-  } catch (error) {
-    console.error("Error", error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-}
-
-// Função para lidar com a solicitação GET para obter um curso específico
+//
+// import { db } from "@/lib/db";
+// import { NextResponse } from "next/server";
+// import { auth } from "@clerk/nextjs/server";
+//
+// // inscreve um usuário em um curso
+// export async function POST(
+//   req: Request,
+//   { params }: { params: { courseId: string } }
+//
+// ) {
+//   try {
+//
+//     const { userId } = await req.json();
+//     const { courseId } = params;
+//
+//     const existingUser = await db.users.findUnique({
+//       where: {
+//         userId: userId
+//       },
+//       include: {
+//         subscribedCourses: true
+//       }
+//     })
+//
+//     if (!existingUser) return new NextResponse("usuário não cadastrado", { status: 200 });
+//
+//     // Verifica se o curso já está inscrito pelo usuário
+//     const isCourseSubscribed = existingUser.subscribedCourses.some(course => course.courseId === courseId);
+//     if (isCourseSubscribed) {
+//       return new NextResponse("Usuário já está inscrito neste curso", { status: 409 });
+//     }
+//
+//     const newSubscribedUser = await db.subscribedCourses.create({
+//       data: {
+//         courseId: courseId
+//       }
+//     })
+//
+//     const updateUser = await db.users.update({
+//       where: { userId },
+//       data: {
+//         subscribedCourses: {
+//           connect: {
+//             id: newSubscribedUser.id,
+//           }
+//         }
+//       }
+//     })
+//
+//     return new NextResponse("created", { status: 200 });
+//   } catch (error) {
+//     console.error("Error", error);
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
+//
+// // Função para lidar com a solicitação GET para obter um curso específico
 // export async function GET(
 //   _: Request,
 //   { params }: { params: { courseId: string } }
@@ -71,7 +71,7 @@ export async function POST(
 //
 //     const existingUser = await db.users.findUnique({
 //       where: {
-//         id: userId // FIX
+//         userId: userId
 //       },
 //       include: {
 //         subscribedCourses: true
@@ -92,4 +92,4 @@ export async function POST(
 //     return new NextResponse("Internal Error", { status: 500 });
 //   }
 // }
-
+//
