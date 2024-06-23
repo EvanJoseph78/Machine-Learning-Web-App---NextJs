05d57825acddc157630d4ce05e28aadbf30b31cd
Evan Joseph <evandromariano49@gmail.com>
2024-06-23 10:12:36 -0300

    feat: fix for production - 2

--- app/api/courses/[courseId]/subscribe/route.ts
+++ app/api/courses/[courseId]/subscribe/route.ts
@@ -56,40 +56,40 @@ export async function POST(
 }
 
 // Função para lidar com a solicitação GET para obter um curso específico
-export async function GET(
-  _: Request,
-  { params }: { params: { courseId: string } }
-) {
-  try {
-    const { courseId } = params
-
-    const { userId } = auth();
-
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    const existingUser = await db.users.findUnique({
-      where: {
-        id: userId // FIX
-      },
-      include: {
-        subscribedCourses: true
-      }
-    })
-
-    if (!existingUser) return new NextResponse("usuário não cadastrado", { status: 200 });
-
-    // Verifica se o curso já está inscrito pelo usuário
-    const isCourseSubscribed = existingUser.subscribedCourses.some(course => course.courseId === courseId);
-    if (isCourseSubscribed) {
-      return new NextResponse("true", { status: 200 });
-    }
-
-    return new NextResponse("false", { status: 200 });
-  } catch (error) {
-    console.error("[COURSES]", error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-}
+// export async function GET(
+//   _: Request,
+//   { params }: { params: { courseId: string } }
+// ) {
+//   try {
+//     const { courseId } = params
+//
+//     const { userId } = auth();
+//
+//     if (!userId) {
+//       return new NextResponse("Unauthorized", { status: 401 });
+//     }
+//
+//     const existingUser = await db.users.findUnique({
+//       where: {
+//         id: userId // FIX
+//       },
+//       include: {
+//         subscribedCourses: true
+//       }
+//     })
+//
+//     if (!existingUser) return new NextResponse("usuário não cadastrado", { status: 200 });
+//
+//     // Verifica se o curso já está inscrito pelo usuário
+//     const isCourseSubscribed = existingUser.subscribedCourses.some(course => course.courseId === courseId);
+//     if (isCourseSubscribed) {
+//       return new NextResponse("true", { status: 200 });
+//     }
+//
+//     return new NextResponse("false", { status: 200 });
+//   } catch (error) {
+//     console.error("[COURSES]", error);
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
 
