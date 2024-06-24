a7c8b66d1de86ee1fe0775d41274c821a2879c4e
Evan Joseph <evandromariano49@gmail.com>
2024-06-24 16:30:12 -0300

    fix: d2

--- app/api/courses/[courseId]/class/current-class/route.ts
+++ app/api/courses/[courseId]/class/current-class/route.ts
@@ -1,18 +1,18 @@
-//
-// import { db } from "@/lib/db";
-// import { NextRequest, NextResponse } from "next/server";
-// import { auth } from "@clerk/nextjs/server"
-//
-// export async function POST(
-//   req: NextRequest,
-//   { params }: { params: { courseId: string } }
-// ) {
-//   try {
-//     const { currentClassId } = await req.json();
-//
-//     return new NextResponse("ok", { status: 200 });
-//   } catch (error) {
-//     return new NextResponse("Internal Error", { status: 500 });
-//   }
-// }
-//
+
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server"
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { currentClassId } = await req.json();
+
+    return new NextResponse("ok", { status: 200 });
+  } catch (error) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
