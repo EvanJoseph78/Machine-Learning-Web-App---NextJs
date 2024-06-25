65052bc075c76b0f6cfddc78609f2f87c9f08602
Evan Joseph <evandromariano49@gmail.com>
2024-06-25 03:12:15 -0300

    feat: homepage

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,61 @@
+"use client"
+
+import { Button } from '@/components/ui/button';
+import React, { useState, useEffect } from 'react';
+import { AuthorsList } from '../_components/authors-list';
+import Footer from '../_components/footer';
+
 const Home = () => {
+  // Estado para armazenar os autores do projeto
+  const [authors, setAuthors] = useState([]);
+
+  // Função para buscar os autores do projeto (pode ser de uma API ou arquivo JSON)
+  useEffect(() => {
+    // Simulando uma chamada de API para buscar os autores
+    const fetchAuthors = async () => {
+      const response = await fetch('/api/authors'); // Ajuste o caminho conforme necessário
+      const data = await response.json();
+      setAuthors(data);
+    };
+
+    fetchAuthors();
+  }, []);
+
   return (
-    <div className="flex h-full ">
-      Home Page
+    <div className='flex justify-center items-center flex-col gap-8 w-screen'>
+      <header className='flex flex-col justify-center items-center py-16 px-2 gap-4'>
+        <h1 className='text-4xl font-semibold text-center'>
+          Sejam muito bem-vindos
+        </h1>
+        <p className='max-w-[600px] text-center'>
+          Acesse cursos gratuitos sobre tecnologia criados por alunos para alunos.
+        </p>
+        <div className='space-x-2'>
+          <Button>Iniciar</Button>
+          <Button variant={"ghost"} className='border'>Login</Button>
+        </div>
+      </header>
+
+      <div className='px-2 text-center'>
+        <h1 className="text-3xl font-bold mb-4">Sobre a Plataforma</h1>
+
+        <p className="text-lg mb-8 text-center max-w-2xl">
+          O nosso projeto tem como objetivo oferecer cursos de
+          Machine Learning para a comunidade interna e externa de Castanhal.
+          Na plataforma, os usuários podem acessar esses cursos de forma gratuita.
+          No futuro, pretendemos democratizar ainda mais a criação de cursos, permitindo que qualquer aluno
+          interessado possa criar e disponibilizar seus próprios cursos na plataforma.
+        </p>
+
+      </div>
+
+      <div className='flex justify-center items-center w-screen px-16 flex-col border-t py-8'>
+        <h1 className="text-3xl font-bold mb-4">Autores do projeto</h1>
+        <AuthorsList></AuthorsList>
+      </div>
+
+      <Footer></Footer>
+
     </div>
   );
 };
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full dark:bg-dark-color">
 
-      <div className="h-[80px] fixed inset-y-0 w-full z-50 bg-gray-50 dark:bg-dark-color ">
+      <div className="h-[80px] fixed inset-y-0 w-screen md:w-full z-50 bg-gray-50 dark:bg-dark-color ">
         <Navbar />
       </div>
 
